1e48acc7e907e865d2bbde07d72c873064d62890
Desmond Dela Dotse <63352385+dela3dotse@users.noreply.github.com>
2025-07-19 23:17:18 +0000

    Added an argument function

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+function arg_1(){
+  console.log("No argument");
+}
+function arg_2(name){
+  console.log("Argument found")
+}
+function arg_3(name, age){
+  console.log("Arguments found")
+}
+arg_1();
+arg_2("John")
+arg_3("Max", 20)
