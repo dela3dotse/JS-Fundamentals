9529d7a2ac21aaa905a7946bd73745f23fdbd13a
Desmond Dela Dotse <63352385+dela3dotse@users.noreply.github.com>
2025-07-19 23:40:43 +0000

    added a new file

---
+++ 3-value_argument.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // Exclude node and script path
+
+if (args.value = 0) {
+  console.log("No argument");
+} else if (args.value = 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
