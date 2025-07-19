631f3cc47a5b72a0062f2031a763a618763de36f
Desmond Dela Dotse <63352385+dela3dotse@users.noreply.github.com>
2025-07-19 23:33:59 +0000

    fixed bug

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,14 +1,15 @@
 function arg_1(){
   console.log("No argument");
 }
+arg_1()
 function arg_2(name){
   console.log("Argument found")
 }
+arg_2("Best")
 function arg_3(name, age, color){
   console.log("Arguments found")
 }
 arg_3("Best", "School", "Tema")
 
-// arg_1();
-arg_2("Best")
+
 
